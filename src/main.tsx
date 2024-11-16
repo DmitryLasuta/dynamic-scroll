aa04fee56ff35c25d0f57499ba255179210f9480
Дмитрий Ласута <dmitriy.lasuta@uptrade.ru>
2024-11-17 01:06:21 +0300

    feat: added react-query instead of axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './App.tsx';
+import { App } from './app.tsx';
 
 import './index.css';
+import { Providers } from './providers/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </StrictMode>
 );
